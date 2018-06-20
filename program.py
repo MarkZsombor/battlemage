7b2cfbbd0d1327bc0466472ba5b13bca4097db60
Mark Zsombor <markszsombor@gmail.com>
2018-06-19 20:13:55 -0700

    basic program structure

--- program.py
+++ program.py
@@ -1,1 +1,28 @@
-# Program will allow 2 players to fight as wizards.
+
+def main():
+    print_header()
+    game_loop()
+
+
+def print_header():
+    print('          Battlemage')
+
+
+def game_loop():
+
+    while True:
+
+        cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around?')
+        if cmd == 'a':
+            print('a')
+        elif cmd == 'r':
+            print('r')
+        elif cmd == 'l':
+            print('l')
+        else:
+            print('Exiting Game')
+            break
+
+
+if __name__ == '__main__':
+    main()
